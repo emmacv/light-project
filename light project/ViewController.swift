3083c1b917572aabbe8636a72b103665cdb5297a
Emmanuel Canto Vazquez <emmanuel.canto@cat.com>
2025-04-02 14:43:16 -0600

    Initial Commit

---
+++ light project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  light project
+//
+//  Created by emmanuel.canto on 02/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ light projectTests/light_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  light_projectTests.swift
+//  light projectTests
+//
+//  Created by emmanuel.canto on 02/04/25.
+//
+
+import Testing
+@testable import light_project
+
+struct light_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
